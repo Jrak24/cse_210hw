7b09ea01d247880d54012e5ba569dc952e54aac6
05jdd <jsornsomrej@gmail.com>
2024-09-28 14:14:00 +0300

    Staged the incorrect

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,41 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Prompt the user for their grade percentage
         Console.WriteLine("What is your grade percentage? ");
         string answer = Console.ReadLine();
-        int percent = int.Parse(answer);
+        
+        // Try parsing the input, handle invalid input gracefully
+        try
+        {
+            int percent = int.Parse(answer);
+            string letter = "";
 
-        string letter = "";
+            // Determine the letter grade based on percentage
+            if (percent >= 90)
+            {
+                letter = "A";
+            }
+            else if (percent >= 80)
+            {
+                letter = "B";
+            }
+            else if (percent >= 75)
+            {
+                letter = "C-";
+            }
+            else if (percent >= 70)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
 
-        if (percent >= 90)
-        {
-            letter = "A";
-        }
-        else if (percent >= 80)
-        {
-            letter = "B".
-        }
-        else if (percent >=75)
-        {
-            letter = "C-";
-        }
-        else if (percent >= 70)
-        {
-            letter = "D"
-        }
-        else{
-            letter = "F";
-        }
+            // Output the letter grade
+            Console.WriteLine($"Your grade is: {letter}");
 
-        Console.WriteLine($"Your grade is: {letter}");
-
-        if (percent >= 75)
-        {
-            Console.WriteLine("You're lucky you passed!");
+            // Display a message based on the grade
+            if (percent >= 75)
+            {
+                Console.WriteLine("You're lucky you passed!");
+            }
+            else
+            {
+                Console.WriteLine("Do better next time!");
+            }
         }
-        else
+        catch (FormatException)
         {
-            Console.WriteLine("Do better next time!");
+            // If input is not a valid number, show an error message
+            Console.WriteLine("Invalid input! Please enter a valid number for the grade percentage.");
         }
     }
 }
